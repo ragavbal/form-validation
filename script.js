904a051b8f9d213eda65b6e9884770a857bc3aec
Balajii Ragavendar J <balajiragav27@gmail.com>
2026-02-14 17:47:53 +0530

    Show inline validation errors for form fields

--- script.js
+++ script.js
@@ -2,20 +2,24 @@ const form=document.getElementById("signupForm");
 form.addEventListener("submit", function(e){
     e.preventDefault();
 
-    const nameInput=document.getElementById("name");
-if(nameInput.value.trim()==="")
-{
-    alert("Name Required!");
-    return;
-}
-
+const nameInput=document.getElementById("name");
+const nameError=document.getElementById("nameError");
+const emailerror=document.getElementById("emailerror");
 const emailInput = document.getElementById("email");
 
+//reset error
+nameError.textContent="";
+emailerror.textContent="";
+
 if (emailInput.value.trim() === "") {
-  alert("Email is required");
+  emailerror.textContent="email is required";
   return;
 }
-
-    console.log("Form Submitted");
+if(nameInput.value.trim()==="")
+{
+    nameError.textContent="Name is required";
+    return;
+}
+console.log("Form Submitted");
 })
 

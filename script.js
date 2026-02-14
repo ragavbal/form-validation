9f3e39f904b4d38310d80ae35558c8a739059efa
Balajii Ragavendar J <balajiragav27@gmail.com>
2026-02-14 17:27:35 +0530

    Name and email validation added

--- script.js
+++ script.js
@@ -1,5 +1,21 @@
 const form=document.getElementById("signupForm");
 form.addEventListener("submit", function(e){
     e.preventDefault();
+
+    const nameInput=document.getElementById("name");
+if(nameInput.value.trim()==="")
+{
+    alert("Name Required!");
+    return;
+}
+
+const emailInput = document.getElementById("email");
+
+if (emailInput.value.trim() === "") {
+  alert("Email is required");
+  return;
+}
+
     console.log("Form Submitted");
 })
+
